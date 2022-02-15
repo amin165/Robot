f576e9c8331f45ad656daa8f36f866c151d23f7a
Amina Luvsanchultem <aminaluv10@gmail.com>
2022-02-15 17:17:24 -0500

    added indexer subsystem

--- src/main/java/frc/robot/commands/ShooterCommand.java
+++
@@ -1,8 +0,0 @@
-package frc.robot.commands;
-
-import frc.robot.subsystems.ShooterSubsystem;
-import edu.wpi.first.wpilibj2.command.CommandBase;
-
-public class ShooterCommand {
-
-}
--- src/main/java/frc/robot/subsystems/IndexerSubsystem.java
+++ src/main/java/frc/robot/subsystems/IndexerSubsystem.java
@@ -1,5 +1,29 @@
 package frc.robot.subsystems;
 
+package frc.robot.subsystems;
+
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.can.VictorSPX;
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+import edu.wpi.first.wpilibj.Encoder;
+import edu.wpi.first.wpilibj.controller.PIDController;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
+import frc.robot.Util;
+import jdk.jfr.Percentage;
+
+import java.util.function.DoubleSupplier;
+
 public class IndexerSubsystem {
 
+    public IndexerSubsystem() {
+    }
+
+    public void run() {
+
+    }
+
+    public void reverse() {
+
+    }
 }
